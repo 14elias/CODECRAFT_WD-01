a895e07bfe4ecbbe88af717cbc8b2305dfa0d8f1
14elias <ellamebrahtom1995@gmail.com>
2025-04-02 20:41:53 +0300

    setting modified

--- userauth/settings.py
+++ userauth/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authApp',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 # Internationalization
