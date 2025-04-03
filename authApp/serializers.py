3c6ec176de9112fb084abca4d2abc9f62fec3384
14elias <ellamebrahtom1995@gmail.com>
2025-04-03 14:19:31 +0300

    userserializer modified

--- authApp/serializers.py
+++ authApp/serializers.py
@@ -5,3 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['id','username','email','first_name','last_name']
+    
+    def create(self, validated_data):
+        user=User.objects.create_user(**validated_data)
+        return user
