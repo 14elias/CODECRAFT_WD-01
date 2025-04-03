82b96b5347f7cdbb8b3a943d6f0da6641a2d5d2c
14elias <ellamebrahtom1995@gmail.com>
2025-04-03 14:21:03 +0300

    default authentication changed to IsAuthenticated

--- userauth/urls.py
+++ userauth/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authApp.urls')),
+    path('api/', include('authApp.urls')),
 ]
