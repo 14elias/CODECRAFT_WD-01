2dd3144c2879c752567f243133928679baa16028
14elias <ellamebrahtom1995@gmail.com>
2025-04-03 14:18:30 +0300

    url modification

--- authApp/urls.py
+++ authApp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('', HomeView.as_view(), name='home_view'),
+    path('register/', RegisterView.as_view(), name='register_view'),
 
 ]
