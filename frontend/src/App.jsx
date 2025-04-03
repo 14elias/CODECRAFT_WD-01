920ff213dfc72c5d29ef9d73e81686dc2ace415a
14elias <ellamebrahtom1995@gmail.com>
2025-04-03 17:05:53 +0300

    frontend customization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import Login from "./route/Login"
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello everone</h1>
-    </>
+      <Router>
+        <Routes>
+          <Route path="login/" element={<Login></Login>}/>
+        </Routes>
+      </Router>
   )
 }
 
