bee0ac568b6bdfb0d1eea8ac27534a8a97765a6c
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 22:13:54 +0300

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Login from "./route/Login"
+import Home from "./route/Home";
+import Register from "./route/Register";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="login/" element={<Login></Login>}/>
+          <Route path="home/" element={<Home/>}/>
+          <Route path="register/" element={<Register/>}/>
         </Routes>
       </Router>
   )
