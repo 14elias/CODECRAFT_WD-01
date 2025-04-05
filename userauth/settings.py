137e615aa65b62622694b09a1a8a73997389378a
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 16:34:08 +0300

    cors header implemented

--- userauth/settings.py
+++ userauth/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'authApp',
     'rest_framework_simplejwt',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,6 +133,12 @@ AUTH_USER_MODEL='authApp.User'
 
 STATIC_URL = 'static/'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite default port
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
