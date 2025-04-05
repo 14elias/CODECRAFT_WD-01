c264dc148bc3faee27776fff3c20407b403cde5a
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 22:10:52 +0300

    setting modification

--- userauth/settings.py
+++ userauth/settings.py
@@ -104,12 +104,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.AllowAny'
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'authApp.authenticate.Authentication',
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     )
+# }
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'authApp.authenticate.Authentication',
     )
