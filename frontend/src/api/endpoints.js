4ddedff630ff2515a870c9810f2fdc65c2598508
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 16:36:59 +0300

    ...

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -1,12 +1,23 @@
-// src/api.js
 import axios from 'axios';
-import API_BASE_URL from '../constants.js'
+import API_BASE_URL from '../constants.js';
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // Include cookies in requests
 });
 
-export const getUsers = () => api.get('/users');
-
+export const login = async (username, password) => {
+  try {
+    const response = await api.post('/api/token/', {
+      username: username,
+      password: password,
+    });
+    return response.data; // Return the response data (e.g., tokens)
+  } catch (error) {
+    console.error('Login failed:', error);
+    throw error; // Rethrow the error for handling in the calling code
+  }
+};
