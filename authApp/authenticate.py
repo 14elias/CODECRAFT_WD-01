3a888e3406b06e9b3407f110175e7d9ce528d4b7
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 22:06:57 +0300

    slight modification custom authentication

--- authApp/authenticate.py
+++ authApp/authenticate.py
@@ -3,13 +3,12 @@ from rest_framework.exceptions import AuthenticationFailed
 class Authentication(JWTAuthentication):
     def authenticate(self, request):
         access_token=request.COOKIES.get('access_token')
-
         if not access_token:
             return None
-        validated_token=self.get_validated_token(access_token)
-
         try:
+            validated_token=self.get_validated_token(access_token)
             user=self.get_user(validated_token)
+            return (user,validated_token)
         except:
-            return AuthenticationFailed('invalid or authentication failed')
-        return (user,validated_token)
+            return None
+        
