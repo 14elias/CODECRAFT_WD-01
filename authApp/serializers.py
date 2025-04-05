b745a0c3c2e901930b763923827b791127f3a75e
14elias <ellamebrahtom1995@gmail.com>
2025-04-05 22:09:31 +0300

    added password hashing

--- authApp/serializers.py
+++ authApp/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True) 
     class Meta:
         model=User
-        fields=['id','username','email','first_name','last_name']
+        fields=['id','username','email','first_name','last_name','password']
     
     def create(self, validated_data):
-        user=User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)  
+        user.save()
         return user
