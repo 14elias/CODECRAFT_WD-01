36c3c21144cbee17972499a00601db6f49837a82
14elias <ellamebrahtom1995@gmail.com>
2025-04-06 22:46:11 +0300

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Login from "./route/Login"
 import Home from "./route/Home";
 import Register from "./route/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
+import PasswordResetConfirm from "./components/PasswordResetConfirm";
+import PasswordResetRequest from "./components/PasswordResetRequest";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
           <Route path="login/" element={<Login></Login>}/>
           <Route path="home/" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
           <Route path="register/" element={<Register/>}/>
+          <Route path="/password-reset" element={<PasswordResetRequest />} />
+          <Route path="/reset-password/:uidb64/:token" element={<PasswordResetConfirm />} />
         </Routes>
       </Router>
   )
