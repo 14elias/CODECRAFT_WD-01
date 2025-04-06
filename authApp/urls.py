7dcacb8854b96a10f8332536464f3d35d27c7eb9
14elias <ellamebrahtom1995@gmail.com>
2025-04-06 22:44:33 +0300

    url for reset password

--- authApp/urls.py
+++ authApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home_view'),
     path('register/', RegisterView.as_view(), name='register_view'),
     path('logout/', LogoutView.as_view(), name='logout_view'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 ]
